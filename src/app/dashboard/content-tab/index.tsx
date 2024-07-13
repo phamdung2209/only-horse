bf95e196eea0f45e630897dc7d4ad07661aa2411
phamdung2209 <phamdung.22092003@gmail.com>
2024-07-13 16:39:46 +0700

    Build ui Dashboard page #1

---
+++ src/app/dashboard/content-tab/index.tsx
@@ -0,0 +1,72 @@
+'use client'
+
+import { useState } from 'react'
+import UnderlineText from '~/components/decorators/underline-text'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '~/components/ui/card'
+import { Label } from '~/components/ui/label'
+import { RadioGroup, RadioGroupItem } from '~/components/ui/radio-group'
+import { Textarea } from '~/components/ui/textarea'
+
+const ContentTab = () => {
+    console.log('render ContentTab')
+    const [textareaValue, setTextareaValue] = useState<string>('')
+    const [mediaType, setMediaType] = useState<'video' | 'image'>('video')
+    const [isPublic, setIsPublic] = useState<boolean>(false)
+    const [mediaUrl, setMediaUrl] = useState<string>('')
+
+    return (
+        <>
+            <p className="text-3xl my-5 font-bold text-center uppercase">
+                <UnderlineText className="decoration-wavy">Share</UnderlineText> Post
+            </p>
+
+            <form action="">
+                <Card className="w-full max-w-md mx-auto">
+                    <CardHeader>
+                        <CardTitle className="text-2xl">New Post</CardTitle>
+
+                        <CardDescription>
+                            Share your exclusive content with your audience. Select only one
+                            video/image at a time.
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent className="grid gap-4">
+                        <div className="grid gap-2">
+                            <Label htmlFor="content">Content</Label>
+                            <Textarea
+                                id="content"
+                                placeholder="Share today’s exclusive content with your audience."
+                                required
+                                value={textareaValue}
+                                onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+                                    setTextareaValue(e.target.value)
+                                }
+                            />
+                        </div>
+
+                        <Label>Media Type</Label>
+                        <RadioGroup>
+                            <Label
+                                htmlFor="video"
+                                className="flex items-center space-x-2 cursor-pointer w-fit"
+                            >
+                                <RadioGroupItem value="video" id="video" />
+                                <p>Video</p>
+                            </Label>
+
+                            <Label
+                                htmlFor="image"
+                                className="flex items-center space-x-2 cursor-pointer w-fit"
+                            >
+                                <RadioGroupItem value="image" id="image" />
+                                <p>Image</p>
+                            </Label>
+                        </RadioGroup>
+                    </CardContent>
+                </Card>
+            </form>
+        </>
+    )
+}
+
+export default ContentTab
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,32 @@
 import BaseLayout from '~/components/base-layout'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '~/components/ui/tabs'
+import ContentTab from './content-tab'
 
 const Dashboard = () => {
-    return <BaseLayout>Dashboard</BaseLayout>
+    console.log('render Dashboard')
+
+    return (
+        <BaseLayout renderRightPanel={false}>
+            <Tabs defaultValue="content" className="w-full mx-auto my-10 px-2 md:px-10">
+                <TabsList className="flex flex-col sm:flex-row w-full mx-auto h-auto">
+                    <TabsTrigger value="content" className="w-full">
+                        Content
+                    </TabsTrigger>
+                    <TabsTrigger value="store" className="w-full">
+                        Store
+                    </TabsTrigger>
+                    <TabsTrigger value="analytics" className="w-full">
+                        Analytics
+                    </TabsTrigger>
+                </TabsList>
+                <TabsContent value="content">
+                    <ContentTab />
+                </TabsContent>
+                <TabsContent value="store">Change your password here.</TabsContent>
+                <TabsContent value="analytics">Analytics</TabsContent>
+            </Tabs>
+        </BaseLayout>
+    )
 }
 
 export default Dashboard
